239666c0aac2dfe81a8d0a19ede796eb14489a48
Homayoun Samavatian <homayoun@TImeLine.attlocal.net>
2016-10-09 03:02:25 -0700

    stepdefinition content added

--- src/test/java/com/stepdefinition/FirstStepDefinition.java
+++ src/test/java/com/stepdefinition/FirstStepDefinition.java
@@ -1,29 +1,33 @@
 package com.stepdefinition;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 
 public class FirstStepDefinition {
+	WebDriver driver;
 	@Given("^I initilize the firefox browser$")
 	public void i_initilize_the_firefox_browser() throws Throwable {
-	   
+	   driver=new FirefoxDriver();
 	}
 
 	@Given("^I am on google homepage$")
 	public void i_am_on_google_homepage() throws Throwable {
-	    
+	    driver.get("http://google.com");
 	}
 
 	@Then("^I validate the title of the page$")
 	public void i_validate_the_title_of_the_page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	 Assert.assertTrue(driver.getTitle().equals("google"));
 	}
 
 	@Then("^I quit the browser$")
 	public void i_quit_the_browser() throws Throwable {
-	   
+	   driver.quit();
 	}
 
 
