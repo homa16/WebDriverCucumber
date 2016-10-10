8ffdb46f90cad589ad1bc3b24788206d4f6d6a54
Homayoun Samavatian <homayoun@TImeLine.local>
2016-10-10 14:02:46 -0700

    test-output ignored

--- out.html/report.js
+++ out.html/report.js
@@ -38,28 +38,28 @@ formatter.match({
   "location": "FirstStepDefinition.i_initilize_the_firefox_browser()"
 });
 formatter.result({
-  "duration": 4405895966,
+  "duration": 4924808091,
   "status": "passed"
 });
 formatter.match({
   "location": "FirstStepDefinition.i_am_on_google_homepage()"
 });
 formatter.result({
-  "duration": 1622922459,
+  "duration": 2909148585,
   "status": "passed"
 });
 formatter.match({
   "location": "FirstStepDefinition.i_validate_the_title_of_the_page()"
 });
 formatter.result({
-  "duration": 14961142,
+  "duration": 11897274,
   "status": "passed"
 });
 formatter.match({
   "location": "FirstStepDefinition.i_quit_the_browser()"
 });
 formatter.result({
-  "duration": 130001132,
+  "duration": 125673935,
   "status": "passed"
 });
 });
